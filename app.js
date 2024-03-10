224ac73715539c67dfff2692928fa2cfac51e322
Vlad <vladstavila@gmail.com>
2024-03-10 10:56:47 +0200

    multer diskStorage dest change

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ app.use(passport.initialize());
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "var/data");
+    cb(null, "/var/data");
   },
   filename: (req, file, cb) => {
     const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
