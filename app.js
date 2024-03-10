49749e726b3ed0237e7134401cb6476f4a2875d0
Vlad <vladstavila@gmail.com>
2024-03-10 11:06:18 +0200

    multer diskStorage dest change

--- app.js
+++ app.js
@@ -37,7 +37,8 @@ const db = new pg.Client({
 app.use(cors());
 
 app.use(express.json());
-app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+// app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+app.use(express.static("/var/data"));
 
 app.use(passport.initialize());
 
